f3d2b3fcaadd9fb43d7fc23a6312d097c6491a75
newcomer119 <mitarth.125035@stu.upes.ac.in>
2024-12-13 16:00:21 +0530

    made changes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,14 +2,24 @@ const express = require("express");
 const router = express.Router();
 const { createOrder, handlePaymentSuccess } = require("../controllers/paymentController");
 const admin = require("firebase-admin");
-const serviceAccount = require("../config/yoga-retreat-cee11-firebase-adminsdk-j16vu-2e9d44168e.json"); // Update with your actual path // Update with your service account key path
 
-// Initialize Firebase
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const db = admin.firestore(); // Initialize Firestore
 
